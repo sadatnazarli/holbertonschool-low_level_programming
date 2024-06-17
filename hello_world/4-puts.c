5fa9707126c014402346e4f0430bf93f4f1aa9d6
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-17 10:54:27 +0000

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+/**
+* main - function
+*
+* Return: void
+**/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
