40a10e0eeba23ae775c013a7404e4dd7fafb3b5d
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-21 13:57:34 +0000

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the single digit numbers of base 10,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
