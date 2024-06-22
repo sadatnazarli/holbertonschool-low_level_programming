533df6dc56804ebf201e0b26f6393f5f7b244fa9
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-22 11:14:08 +0000

    task0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
