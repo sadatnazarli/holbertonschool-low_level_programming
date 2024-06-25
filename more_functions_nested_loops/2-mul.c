33bbf231e08597c5c2f7b8bfde6bfa15b4895932
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-25 15:08:57 +0000

    task2 final

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- * main -check the code.
- * Return: Always 0.
- **/
-int mul (int a, int b)
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
 {
 	return a + b;
-}
-int main(void)
-{
-	printf("%d\n", mul(98, 1024));
-	return (0);
 }
