5c98f1c57d4bb159cd9a1303fb1ed31162c28454
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-25 17:40:56 +0000

    task4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ * print_most_numbers - function or entry point
+ * Return: expected result
+ **/
 
 void print_most_numbers(void)
 {
