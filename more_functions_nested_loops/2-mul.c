dbb4dbcdda8fdd6cbbbadc5e4aed9420186715b7
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-25 15:01:37 +0000

    task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -check the code.
+ * Return: Always 0.
+ **/
+int mul (int a, int b)
+{
+	return a + b;
+}
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	return (0);
+}
