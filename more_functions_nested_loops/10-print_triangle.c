f4cb4acdb1bb7216592110d656407f8df79287c5
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-26 16:30:13 +0000

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle - function or entry point
+ * @size: character that will be checked
+ **/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j >= 1; j--)
+			{
+				if (i < j)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
