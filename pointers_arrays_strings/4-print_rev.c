e06bdce3118240b142ff2e88db7fb9425c72c322
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-29 10:29:54 +0000

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,17 @@ void print_rev(char *s)
 	int len = strlen(s);
 	int i;
 
-	printf("The reversed string is :");
+	char *msg = "The reversed string is: ";
+
+	for (i = 0; msg[i] != '\0'; i++)
+	{
+		_putchar(msg[i]);
+	}
+
 	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
+
 	_putchar('\n');
 }
