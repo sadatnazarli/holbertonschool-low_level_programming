638007edd86704b9489edc59edc46ea7fa21e562
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-29 10:44:20 +0000

    task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string in place
+ * @s: the string to be reversed
+ */
+void rev_string(char *s)
+{
+	int length = strlen(s);
+
+	int a, b;
+
+	for (a = 0, b = length - 1; a < b; a++, b--)
+	{
+		char temp = s[a];
+
+		s[a] = s[b];
+		s[b] = temp;
+	}
+}
