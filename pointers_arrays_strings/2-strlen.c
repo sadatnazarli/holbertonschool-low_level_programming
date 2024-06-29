1254e23772c73368719f25426582773186c56fa1
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-29 06:02:13 +0000

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+ * _strlen - Sətirin uzunluğunu hesablayan funksiya
+ * @s: Hesablanacaq sətr
+ *
+ * Return: Sətrin uzunluğu (null simvolu daxil olmaz)
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
