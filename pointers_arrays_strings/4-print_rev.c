d8681b4be4586a4a786708138927f536afe5d647
sadatnazarli <sedat.nazarli@gmail.com>
2024-06-29 10:25:22 +0000

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
-*  print_rev - prints reverse of the string followed
-* by a new line to standart output
-* @str: string parameter
-**/
+ * print_rev - prints the reverse of the string by a new line
+ * @s: string parameter
+ */
 void print_rev(char *s)
 {
 	int len = strlen(s);
 	int i;
 
-	printf("The reversed string is %d");
-	for (i = len-1; i >= 0; i--)
+	printf("The reversed string is :");
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
