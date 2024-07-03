e2dc6152a72825bd8a8fb9666370579de22e6c01
sadatnazarli <sedat.nazarli@gmail.com>
2024-07-03 14:58:14 +0000

    task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-/** This code has entery vale
- * And return type
- */
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: where memory area pointed to by
+ * @n: first n bytes of the memory area
+ * @b: constant bytes
+ * Return: a pointer to the memory area s
+ **/
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	for ( i = 0; i < n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-	return s;
+
+	return (s);
 }
