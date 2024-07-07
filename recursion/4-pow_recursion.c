a50514327e1d18ee8ec553d112909024877354bc
sadatnazarli <sedat.nazarli@gmail.com>
2024-07-07 06:37:03 +0000

    task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns x raised to the power of y
+ * @x: The base number
+ * @y: The exponent
+ *
+ * Return: The result of x raised to the power of y,
+ *         or -1 if y is less than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
