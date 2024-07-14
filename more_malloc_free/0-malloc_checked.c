31289584e8a993b93094a402692d4521ec9c70b2
sadatnazarli <sedat.nazarli@gmail.com>
2024-07-14 10:46:19 +0000

    task 0 fixed betty style problem

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,13 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
- * malloc_checked - allocates memory using malloc
- * @b: amount of memory to allocate
- *
- * Return: pointer to the allocated memory
- * if malloc fails, causes normal process
- * termination with status value of 98
- */
+ * malloc_checked - allocates memory
+ * Return: a pointer to the alloacated memory
+ * @b: unsigned int
+ **/
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-	ptr = malloc(b);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
