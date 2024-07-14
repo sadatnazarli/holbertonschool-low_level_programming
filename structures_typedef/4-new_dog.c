4b33d352bc976ee500043da2b1a5144b929583ff
sadatnazarli <sedat.nazarli@gmail.com>
2024-07-14 10:41:21 +0000

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,42 +1,49 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog or NULL
+ * Return: pointer to the new dog or NULL if function fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr_dog;
-	int lname, lowner;
+	char *new_name;
+	char *new_owner;
 
 	if (name == NULL || owner == NULL)
+	{
 		return (NULL);
+	}
 
 	ptr_dog = malloc(sizeof(dog_t));
-
 	if (ptr_dog == NULL)
-		return (NULL);
-
-	lname = strlen(name);
-	lowner = strlen(owner);
-
-	ptr_dog->name = malloc(lname + 1);
-	ptr_dog->owner = malloc(lowner + 1);
-
-	if (ptr_dog->name == NULL || ptr_dog->owner == NULL)
 	{
-		free(ptr_dog->name);
-		free(ptr_dog->owner);
+		return (NULL);
+	}
+	new_name = malloc(strlen(name) + 1);
+	if (new_name == NULL)
+	{
 		free(ptr_dog);
 		return (NULL);
 	}
-	strcpy(ptr_dog->name, name);
-	strcpy(ptr_dog->owner, owner);
+	strcpy(new_name, name);
+
+	new_owner = malloc(strlen(owner) + 1);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(ptr_dog);
+		return (NULL);
+	}
+	strcpy(new_owner, owner);
+
+	ptr_dog->name = new_name;
 	ptr_dog->age = age;
+	ptr_dog->owner = new_owner;
 	return (ptr_dog);
 }
