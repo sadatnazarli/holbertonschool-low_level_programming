4ce79f228727556c97de172593753b26e20bb76a
sadatnazarli <sedat.nazarli@gmail.com>
2024-07-31 10:00:35 +0000

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: A pointer to the head of the list_t list.
- *
  * Return: The number of nodes in the list_t list.
  */
 size_t print_list(const list_t *h)
